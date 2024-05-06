c4561285787bfe95cabc51d9d54b0ca0344f7fa9
monir178 <monir.mzs17@gmail.com>
2024-05-07 00:53:15 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl">Unity Uplift</h1>
+      <Button>Click</Button>
+    </>
+  );
+}
+
+export default App;
